7beb0232e175eb89160c02e220161ead198b6ed8
mihukss <mikhail.ryzhov@aviasales.ru>
2023-05-12 14:59:43 +0300

    first_lesson_finished

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,30 @@
+class UsersController < ApplicationController 
+    
+    def show
+      @users = User.all
+    end
+
+    def new
+      @user = User.new
+    end
+
+    def create
+      user = User.new permitted_params
+      user.save!
+    end
+
+    def edit
+      @user = User.find(params[:id])
+    end
+    
+    def update
+      @user = User.find(params[:id])
+      @user.update!(permitted_params)
+    end
+
+    private
+
+    def permitted_params
+      params.require(:user).permit(:email, :password, :first_name)
+    end
+end
