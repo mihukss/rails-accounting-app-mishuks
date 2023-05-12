f4604a7797655dd7e16298691527d06c29067e76
mihukss <mikhail.ryzhov@aviasales.ru>
2023-05-12 15:04:13 +0300

    resource -> resources

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController 
     
-    def show
+    def index
       @users = User.all
     end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  resource :users
+  resources :users
   
 end
