f875daf226fe934d4132e121b1361e695e64d819
Aleksey Frolov <afrolov@taxdome.com>
2023-05-13 17:35:40 +0400

    rails new

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,29 @@
+class UsersController < ApplicationController
+  def index
+    @users = User.where(first_name: nil)
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    user = User.new permitted_params
+    user.save!
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update!(permitted_params)
+  end
+    
+  private
+
+  def permitted_params
+    params.require(:user).permit(:email, :password, :first_name)
+  end
+end
